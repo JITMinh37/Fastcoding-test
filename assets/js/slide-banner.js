23f26bad2eb5703d9454329bfd84f2f0471f2222
JITMinh37 <nhatminhhoangeng22112k3@gmail.com>
2024-09-13 20:10:19 +0700

    init

---
+++ assets/js/slide-banner.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var thoigian;
+    var slides = document.querySelectorAll('.Slide');
+    var dots = document.querySelectorAll('.quanlynutchuyen li');
+    var currentSlideIndex = 0;
+
+
+    function showSlide(index) {
+        if (index >= slides.length) {
+            currentSlideIndex = 0;
+        } else if (index < 0) {
+            currentSlideIndex = slides.length - 1;
+        } else {
+            currentSlideIndex = index;
+        }
+
+        slides.forEach(function(slide) {
+            slide.classList.add('AnSlide');
+            slide.classList.remove('cdSlide');
+            slide.classList.remove('active');
+        });
+
+        dots.forEach(function(dot) {
+            dot.classList.remove('kichhoat');
+        });
+
+        slides[currentSlideIndex].classList.remove('AnSlide');
+        slides[currentSlideIndex].classList.add('cdSlide');
+        slides[currentSlideIndex].classList.add('active');
+        dots[currentSlideIndex].classList.add('kichhoat');
+    }
+
+
+    function autoSlide() {
+        clearInterval(thoigian);
+        thoigian = setInterval(function() {
+            currentSlideIndex++;
+            showSlide(currentSlideIndex);
+        }, 4000);
+    }
+
+
+    for (var i = 0; i < dots.length; i++) {
+        (function(index) {
+            dots[index].addEventListener("click", function() {
+                currentSlideIndex = index;
+                showSlide(currentSlideIndex);
+                clearInterval(thoigian);
+                autoSlide();
+            });
+        })(i);
+    }
+    autoSlide();
+    showSlide(currentSlideIndex);
+},false)
